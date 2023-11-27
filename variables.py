d14e6cdda70c8223a54cb3276b734741786f828d
unknown <kamatabhishek14@gmail.com>
2023-11-27 18:29:27 +0530

    added how to take the inputs from the user

--- variables.py
+++ variables.py
@@ -4,6 +4,14 @@ age = 22
 name = "abhishek1"
 age = 23
 
-# at the same time we can update the variable values too
+# # at the same time we can update the variable values too
 
 print(name,"age is",age)
+
+# o/p --> abhishek1 age is 23
+
+
+# How to take the input from the user
+
+name1 = input("what is your name1?")
+print(name1)
