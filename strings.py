c747b64085f8225a7d94a3bd7b4e8b4a2c5ec9db
unknown <kamatabhishek14@gmail.com>
2023-11-27 19:21:05 +0530

    added examples based on strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+# suppose you want to search for a particular word from a string then you can do
+# Note -: In python also indexes start with 0
+
+# example 1
+
+name = "Abhishek Kamat"
+print(name.find('A'))
+
+# o/p -: 0 (we got the index of A)
+# If we dont find the word the it will return -1
+
+
+# example 2
+
+name = "Abhishek Kamat"
+result = name.replace('Kamat','Patil')
+
+print(result)
+
+# o/p-: Abhishek Patil
+
+
